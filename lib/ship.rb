877f1e836a766b9d885bdece7acd0899537f3f2c
icicleta <icicleta@gmail.com>
2015-03-23 14:26:06 +0000

    add some requests to ship

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,12 +1,14 @@
 # defining class
 class Ship
   attr_accessor :size
+  attr_reader :coords
   def initialize
     @size
     @coords = []
   end
 
   def set_coords(input)
-    fail unless input.is_a?(Array) && (input.length == size)
+    fail 'Requires an array' unless input.is_a?(Array) && (input.length == size)
+    @coords = input
   end
 end
