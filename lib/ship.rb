d6f0658528df90222e9f0dab1e63b9a6aa346b9c
Jade King <dev@jadeking.co.uk>
2015-03-24 11:03:47 +0000

    updated #set_coords to #location_co_ords

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,12 +1,14 @@
 # defining class
 class Ship
   attr_accessor :size
+  attr_reader :coords
   def initialize
-    @size
+    @size = 0
     @coords = []
   end
 
-  def set_coords(input)
-    fail unless input.is_a?(Array) && (input.length == size)
+  def location_coords!(input)
+    fail 'Requires an array' unless input.is_a?(Array) && (input.length == size)
+    @coords = input
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,4 +1,8 @@
 require 'cell'
 describe Cell do
   it { expect(subject).to respond_to :state }
+
+  it 'knows if its empty' do
+    expect(subject).to be_empty
+  end
 end
